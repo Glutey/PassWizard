2f3b465a5e0f1f88eb05514efbac8c26fd6950a3
Glutey <mr_tscreen@hotmail.com>
2023-01-17 14:47:38 +0000

    getRandom function added.

--- script.js
+++ script.js
@@ -113,10 +113,14 @@ function getPasswordOptions() {
         hasSpecial: hasSpecial
     }
 }
+
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
 }
+
+
 
 // Function to generate password with user input
 function generatePassword() {
